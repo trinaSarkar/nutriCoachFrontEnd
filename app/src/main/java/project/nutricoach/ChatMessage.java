a6818cce1483484543b890e8c6e64ab717c44c68
Ana Carolina Mexia <anamexia@stanford.edu>
2017-05-08 18:01:47 -0700

    added app

---
+++ app/src/main/java/project/nutricoach/ChatMessage.java
@@ -0,0 +1,16 @@
+package project.nutricoach;
+
+/**
+ * Created by anacarolinamexia on 5/8/17.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
---
+++ app/src/main/java/project/nutricoach/MainActivity.java
@@ -0,0 +1,83 @@
+package project.nutricoach;
+
+
+
+ import android.app.Activity;
+ import android.database.DataSetObserver;
+ import android.os.Bundle;
+ import android.view.KeyEvent;
+ import android.view.View;
+ import android.widget.AbsListView;
+ import android.widget.Button;
+ import android.widget.EditText;
+ import android.widget.ListView;
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private Button buttonSend;
+    private boolean left = true;
+    private boolean right = false;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        buttonSend = (Button) findViewById(R.id.send);
+
+        listView = (ListView) findViewById(R.id.msgview);
+
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.right);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) findViewById(R.id.msg);
+        chatText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return sendChatMessage();
+                }
+                return false;
+            }
+        });
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                sendChatMessage();
+                sendRightMessage();
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+    }
+
+    private boolean sendRightMessage() {
+        String response= "Sample Response";
+        chatArrayAdapter.add(new ChatMessage(right,response));
+        chatText.setText("");
+        return true;
+    }
+
+    private boolean sendChatMessage() {
+        chatArrayAdapter.add(new ChatMessage(left, chatText.getText().toString()));
+        chatText.setText("");
+        return true;
+    }
+}
